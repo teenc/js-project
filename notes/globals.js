267c20be9233aa8329c0d914637f25252c599e0c
teenc <skellycantsleep@gmail.com>
2026-02-28 01:02:46 +0200

    repushing

--- notes/globals.js
+++
@@ -1,7 +0,0 @@
-// GLOBALS
-
-// __dirname - path to current directory 
-// __filename - name of the file
-// require - function to use modules (CommonJS)
-// module - info about the current module (file)
-// process - info about the env (environment) where the program is being executed
