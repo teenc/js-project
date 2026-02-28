43a8340beef2e0dbb5f3a5779e15450fa1bc70fb
teenc <skellycantsleep@gmail.com>
2026-02-28 15:38:29 +0200

    added functions

---
+++ 03-functions.js
@@ -0,0 +1,20 @@
+// --------
+// A function is a reusable block of code that runs when you call it.
+// --------
+
+// create a function
+function greet() {
+    console.log("hello world");
+}
+
+// call the function
+greet(); // output: hello world
+
+
+// create a function with parameters (inputs)
+function add(a, b) {
+    return a + b; // send the result back
+}
+
+// call the function and print the result
+console.log(add(2, 3)); // output: 5
